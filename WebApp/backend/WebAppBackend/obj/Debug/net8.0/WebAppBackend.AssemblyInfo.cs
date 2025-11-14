16dab04f1764ebb3cbad49453339a8ddcf6c1888
ValentinGabrie <nicolagabriel02@gmail.com>
2025-11-14 18:15:50 +0200

    aaaa

--- WebApp/backend/WebAppBackend/obj/Debug/net8.0/WebAppBackend.AssemblyInfo.cs
+++ WebApp/backend/WebAppBackend/obj/Debug/net8.0/WebAppBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9249488a5f9890c0241f847cdc8dd4d374760c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7243cb33be59efbec124d2f7fed188957a4e1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
