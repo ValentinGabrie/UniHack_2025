3466a14b97ff87fc076897dfbd710248fdeacc48
vladlautaru <vlad.lautaru@student.upt.ro>
2025-11-15 18:09:02 +0200

    forgot context

--- WebApp/backend/WebAppBackend/Controllers/AuthController.cs
+++ WebApp/backend/WebAppBackend/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using WebAppBackend.Data;
 using WebAppBackend.DTO;
 
 namespace WebAppBackend.Controllers;
@@ -8,6 +9,13 @@ namespace WebAppBackend.Controllers;
 [Route("api/[controller]")]
 public class AuthController
 {
+    private readonly ApplicationDbContext _context;
+    
+    public AuthController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
     [HttpPost]
     public void Login(LoginDto loginDto)
     {
